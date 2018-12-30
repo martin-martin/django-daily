917cb0b8a172209a795a04fa5b8060210e1c216e
martin-martin <breuss.martin@gmail.com>
2018-12-30 11:55:43 -0700

    prod

--- dailywords/settings.py
+++ dailywords/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     # SECURITY WARNING: keep the secret key used in production secret!
