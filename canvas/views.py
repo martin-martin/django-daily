5fbf69b55f463808fd9dec3404975a8e0f53611b
martin-martin <breuss.martin@gmail.com>
2020-02-24 04:23:38 +0200

    Refactor app structure

--- canvas/views.py
+++ canvas/views.py
@@ -8,7 +8,7 @@ def write(request):
     if request.method == 'POST':
         form = TextForm(request.POST)
         if not form.is_valid():
-            return render(request, 'canvas.html', {'note': 'error', 'form': form})
+            return render(request, 'canvas/canvas.html', {'note': 'error', 'form': form})
         else:
             text = form.save(commit=False)
             text.author = request.user
@@ -17,4 +17,4 @@ def write(request):
             # TODO: add feedback that the entry was successfully saved
     else:
         form = TextForm(None)
-    return render(request, 'canvas.html', {'form': form})
+    return render(request, 'canvas/canvas.html', {'form': form})
