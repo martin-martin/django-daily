18a6bf381cfcb115d2483175c3ad4f4a171fc4c8
martin-martin <breuss.martin@gmail.com>
2020-02-24 12:40:15 +0200

    Add ListView for 750 words

--- canvas/urls.py
+++ canvas/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from .models import Text
 from .views import write
 
 
 urlpatterns = [
     path('', write),  # could be 'canvas/'
+    path('all/', login_required(ListView.as_view(model=Text))),
 ]
