8a25e6caedf67827e86430d3dcb52906fd79cf20
martin-martin <breuss.martin@gmail.com>
2020-02-24 04:40:37 +0200

    Update to local Postgres DB

--- dailywords/settings.py
+++ dailywords/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     # SECURITY WARNING: keep the secret key used in production secret!
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'dailywords.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PWD'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
