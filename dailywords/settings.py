eee83015c0cf719852def7f191c55695f97d0137
martin-martin <breuss.martin@gmail.com>
2020-03-07 09:05:45 +0200

    Fix: production env

--- dailywords/settings.py
+++ dailywords/settings.py
@@ -10,7 +10,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.environ.get('PRODUCTION'):  # set this on heroku
+    DEBUG = False
+else:
+    DEBUG = True
 
 if DEBUG:
     # SECURITY WARNING: keep the secret key used in production secret!
